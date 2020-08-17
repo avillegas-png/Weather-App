2ba84889ff7a32d74d1d2e5b919059c8ce81b20f
avillegas-png <67976032+avillegas-png@users.noreply.github.com>
2020-08-17 14:39:30 -0500

    Add files via upload

---
+++ Alan_V_weatherApp/Alan_V_weatherApp/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Alan_V_weatherApp
+//
+//  Created by Alan Villegas on 8/17/20.
+//  Copyright © 2020 Alan Villegas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        if let url = NSURL(string: "https://api.darksky.net/forecast/ae58c5fa7285b492f6a553d200018d9e/42.5917,88.4334") {
+            if let data = NSData(contentsOf: url as URL){
+                do {
+                    let parsed = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String:AnyObject]
+                    
+                    let newDict = parsed
+                    
+                    print(newDict["currently"]!["summary"] as Any)
+                    print(newDict["currently"]!["temperature"] as Any)
+                    print(newDict["currently"]!["dewPoint"] as Any)
+                    
+                    self.currentTemp.text = "\(newDict["currently"]!["temperature"]!!)"
+                    self.currentSummary.text = "\(newDict["currently"]!["summary"]!!)"
+                    self.currentlyDewpoint.text = "\(newDict["currently"]!["dewPoint"]!!)"
+                }
+                catch let error as NSError {
+                    print("A JSON parsithng error occurred, here are the details:\n \(error)")
+                }
+            }
+        }
+        
+    }
+
+    @IBOutlet var currentTemp: UILabel!
+    
+    @IBOutlet var currentSummary: UILabel!
+    
+    @IBOutlet var currentlyDewpoint: UILabel!
+}
+
